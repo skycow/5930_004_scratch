d00d5ec5a2ab93337cf2e52d87fded6ac7dd6bda
Skyler Cowley <skyler.cowley@gmail.com>
2017-10-25 20:04:47 -0600

    step=0.03 hid=300 iter=500

--- mnist.py
+++ mnist.py
@@ -2,7 +2,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 #import matplotlib.pyplot as plt
 #import matplotlib as mpl
 import numpy as np
-#import random
+import random
 from datetime import datetime
 
 mnist = input_data.read_data_sets("/tmp/data/")
@@ -15,9 +15,9 @@ Y_test = mnist.test.labels.astype("int")
 
 batch_size = 100
 num_mats = 28*28
-num_hid_neur = 30
+num_hid_neur = 300
 num_outputs = 10
-step_size = 0.4
+step_size = 0.3
 
 X_train_small = X_train[0:batch_size]
 Y_train_small = Y_train[0:batch_size]
@@ -102,32 +102,35 @@ def backprop(mat, y, W):
     return dJdw1, dJdw2
 
 
-for i in range(50):
+for i in range(500):
     #pick a batch
-     #samp_index = random.sample(range(0, X_train.shape[0]-1), batch_size)
-    #
-    # X_train_small = []
-    # for index in samp_index:
-    #     X_train_small.append(X_train[index])
+    samp_index = random.sample(range(0, X_train.shape[0]-1), batch_size)
+    
+    X_train_batch = np.zeros((batch_size,28*28))
+
+    #X_train_batch = []
+    for index in range(0,len(samp_index)-1):
+        X_train_batch[index] = X_train[samp_index[index]]
     # print(X_train_small)
     # print("iter ",i)
     #
-    # Y_train_small_mat = np.zeros((batch_size,num_outputs))
-    # for r in range(batch_size):
-    #     Y_train_small_mat[r][Y_train[samp_index[r]]] = 1
-    X_train_small = X_train[batch_size*i:batch_size+batch_size*i]
-    Y_train_small = Y_train[batch_size*i:batch_size+batch_size*i]
-
-    Y_train_small_mat = np.zeros((batch_size,num_outputs))
-
+    Y_train_batch_mat = np.zeros((batch_size,num_outputs))
     for r in range(batch_size):
-        Y_train_small_mat[r][Y_train_small[r]] = 1
+        Y_train_batch_mat[r][Y_train[samp_index[r]]] = 1
+
+    #X_train_batch = X_train[batch_size*i:batch_size+batch_size*i]
+    #Y_train_batch = Y_train[batch_size*i:batch_size+batch_size*i]
+
+    #Y_train_batch_mat = np.zeros((batch_size,num_outputs))
+
+    #for r in range(batch_size):
+        #Y_train_small_mat[r][Y_train_small[r]] = 1
 
     #print(Y_train_small_mat)
     #print(Y_train_small)
 
     #do back prop
-    dJdw1, dJdw2  = backprop(X_train_small, Y_train_small_mat, W)
+    dJdw1, dJdw2  = backprop(X_train_batch, Y_train_batch_mat, W)
 
     #update weights
     W[0] = W[0] - step_size * dJdw1/np.max(np.fabs(dJdw1))
@@ -153,17 +156,17 @@ comp2 = comp==0
 
 print(np.around(np.sum(comp2)/comp2.shape[1]*100,2))
 
-f = open('results/results'+datetime.now().strftime("%Y%m%d%H%M%S")+'.txt','w')
+##f = open('results/results'+datetime.now().strftime("%Y%m%d%H%M%S")+'.txt','w')
 
 #np.set_printoptions(threshold='nan')
 
-f.write(str(np.around(np.sum(comp2)/comp2.shape[1]*100,2))+"\n")
+##f.write(str(np.around(np.sum(comp2)/comp2.shape[1]*100,2))+"\n")
 
 #f.write(str(W[0]))
 
 #f.write(str(W[1])+"\n")
 
-f.close()
+##f.close()
 
 #print("comp2",comp2)
 
